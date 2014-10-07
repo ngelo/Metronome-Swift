8588c23fdf402e13535c3a7c96a3c612951624b7
Nikolas Gelo <nikolas.gelo@gmail.com>
2014-10-07 16:20:54 -0700

    Initial project creation
    Just created application in Xcode.

---
+++ Metronome-Swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Metronome-Swift
+//
+//  Created by Nikolas Gelo on 10/7/14.
+//  Copyright (c) 2014 Nikolas Gelo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Metronome-SwiftTests/Metronome_SwiftTests.swift
@@ -0,0 +1,36 @@
+//
+//  Metronome_SwiftTests.swift
+//  Metronome-SwiftTests
+//
+//  Created by Nikolas Gelo on 10/7/14.
+//  Copyright (c) 2014 Nikolas Gelo. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Metronome_SwiftTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
