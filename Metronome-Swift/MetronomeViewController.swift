cc5a1260903aad744035893d902794f13d0eac29
Nikolas Gelo <nikolas.gelo@gmail.com>
2014-10-08 03:22:55 -0700

    Added base logic for toggling metronome.

--- Metronome-Swift/MetronomeViewController.swift
+++ Metronome-Swift/MetronomeViewController.swift
@@ -8,21 +8,56 @@
 
 import UIKit
 
-// MARK: - MetronomeViewController
-
 class MetronomeViewController: UIViewController {
     
     @IBOutlet weak var tempoLabel: UILabel!
     @IBOutlet weak var tempoStepper: UIStepper!
     
-    var tempo: Int! /*{
-        set {
-            self.tempo = newValue
+    var metronomeIsOn = false
+    
+    var tempo: Int = 60 {
+        didSet {
             tempoLabel.text = "\(tempo)"
-        } get {
-            return self.tempo
+            tempoStepper.value = Double(tempo)
         }
-    }*/
+    }
+    
+    @IBAction func tempoChanged(var tempoStepper: UIStepper) {
+        // Save the new tempo.
+        tempo = Int(tempoStepper.value)
+    }
+    
+    @IBAction func toggleMetronome(var toggleMetronomeButton: UIButton) {
+        // If the metronome is currently on, stop the metronome and change
+        // the image of the toggle metronome button to the "Play" image and
+        // its tint color to green.
+        if metronomeIsOn {
+            // Mark the metronome as off.
+            metronomeIsOn = false
+            
+            // TODO: Stop the metronome sound.
+            
+            // Change the toggle metronome button's image to "Play" and tint
+            // color to green.
+            toggleMetronomeButton.setImage(UIImage(named: "Play"), forState: .Normal)
+            toggleMetronomeButton.tintColor = UIColor.greenColor()
+        }
+        
+        // If the metronome is currently off, start the metronome and change
+        // the image of the toggle metronome button to the "Start" image and
+        // its tint color to green
+        else {
+            // Mark the metronome as on.
+            metronomeIsOn = true
+            
+            // TODO: Start the metronome sound.
+            
+            // Change the toggle metronome button's image to "Stop" and tint
+            // color to red.
+            toggleMetronomeButton.setImage(UIImage(named: "Stop"), forState: .Normal)
+            toggleMetronomeButton.tintColor = UIColor.redColor()
+        }
+    }
 
     // MARK: - UIViewController
     // MARK: Managing the View
@@ -30,9 +65,8 @@ class MetronomeViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // Set the inital value of the tempo.
         tempo = 120
-        
-        tempoLabel.text = "\(tempo)"
     }
 }
 
